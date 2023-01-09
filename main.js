f48f362ba1c21eb2cd69d12c5b988023e0603d83
its me <76001700+whoiszacky@users.noreply.github.com>
2023-01-09 22:26:45 +0300

    Add files via upload
    incomplete:(

---
+++ main.js
@@ -0,0 +1,45 @@
+const aboutMe = document.querySelector ("#about")
+const projects = document.querySelector("#project")
+const aboutContent = document.querySelector("#about-content")
+const projectsContent = document.querySelector("#projects-content")
+
+aboutMe.addEventListener('click', () => {
+  const aboutBox = new WinBox({
+    title: 'About Me',
+    // modal: true,
+    width: '370px',
+    height: '370px',
+    top: "25%",
+    right: 50,
+    bottom: 50,
+    left: "50%",
+    mount: aboutContent,
+    onfocus: function () {
+      this.setBackground('#a52727')
+    },
+    onblur: function () {
+      this.setBackground('#e27865')
+    },
+  })
+})
+
+
+projects.addEventListener('click', () => {
+  const projectBox = new WinBox({
+    title: 'Projects',
+    // modal: true,
+    width: '370px',
+    height: '370px',
+    top: "30%",
+    right: 50,
+    bottom: 50,
+    left: "60%",
+    mount: projectsContent,
+    onfocus: function () {
+      this.setBackground('#a52727')
+    },
+    onblur: function () {
+      this.setBackground('#e27865')
+    },
+  })
+})
